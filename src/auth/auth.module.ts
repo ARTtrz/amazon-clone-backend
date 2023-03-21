47fee05579157adeb9801849fa7423885354cbef
Artyom Kim <artem_kim_2006@list.ru>
2023-03-21 23:27:27 +0600

    finished auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { PrismaService } from 'src/prisma.service'
+import { JwtService } from '@nestjs/jwt/dist/jwt.service'
+import { JwtStrategy } from './strategies/jwt.strategy'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { getJwtConfig } from 'src/config/jwt.config'
+
+@Module({
+	imports: [
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	],
+	controllers: [AuthController],
+	providers: [AuthService, PrismaService, JwtStrategy]
+})
+export class AuthModule {}
