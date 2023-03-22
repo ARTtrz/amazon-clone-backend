f757347c5faff77494fcbffc93b4a5511469b5ac
Artyom Kim <artem_kim_2006@list.ru>
2023-03-22 19:10:00 +0600

    user finished

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import {
+	Body,
+	Param,
+	Controller,
+	Get,
+	HttpCode,
+	Patch,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import { UserDto } from './user.dto'
+import { UserService } from './user.service'
+
+@Controller('user')
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') id: number) {
+		return this.userService.byId(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Auth()
+	@HttpCode(200)
+	@Put('login')
+	async login(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+		return this.userService.updateProfile(id, dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Patch('profile/favorites/:productId')
+	async toggleFavorite(
+		@Param('productId') productId: number,
+		@CurrentUser('id') id: number
+	) {
+		return this.userService.toggleFav(id, productId)
+	}
+}
